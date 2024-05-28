c3be5d6fe904edeb31dbc99d0af1a7e8860b7a24
TheMilkman21 <140124197+TheMilkman21@users.noreply.github.com>
2024-05-28 05:00:54 +0000

    done

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 from shapes import Shapes, Rectangle, Triangle, Circle
 
-rectangle = Rectangle(10, 3)
-print(f"area is {Rectangle.calc_area()}, perimeter is {Rectangle.calc_perimeter()}")
 
-triangle = Triangle(3, 3)
-print(f"area is {Triangle.calc_area()}, perimeter is {Triangle.calc_perimeter()}")
+def menu():
+    print("welcome to shapes")
+    choice = input("1: Rectangle\n2: Triangle\n3: circle\n:")
+    if choice == "1":
+        rectangle()
+    elif choice == "2":
+        triangle()
+    elif choice == "3":
+        circle()
+    else:
+        
+        
+    
 
-circle = Circle(5)
-print(f"area is {Circle.calc_area()}, perimeter is {Circle.calc_perimeter()}")
+
+def rectangle():
+    width = float(input("enter width: "))
+    height = float(input("enter height: "))
+    rectangle = Rectangle(width, height)
+    print(f"\narea is: {rectangle.calc_area()}\nperimeter is: {rectangle.calc_perimeter()}\n")
+    pause()
+
+def triangle():
+    base = float(input("enter base: "))
+    height = float(input("enter height: "))
+    triangle = Triangle(base, height)
+    print(f"\narea is {triangle.calc_area()}\nperimeter is {triangle.calc_perimeter()}\n")
+    pause()
+
+def circle():
+    radius = float(input("enter radius: "))
+    circle = Circle(radius)
+    print(f"\narea is {circle.calc_area()}\nperimeter is {circle.calc_perimeter()}\n")
+    pause()
+
+def pause():
+    input("\npress enter to continue: \n")
+
+
+while True:
+    menu()
+
+
+
 
